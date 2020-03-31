39953d1b108f44e131cf80eb748c89728a19c4fb
Ryan Bautista <Ryan.Bautista86@myhunter.cuny.edu>
2020-03-31 12:50:25 -0400

    Removed front

--- front/app/src/main/java/com/example/music_compromiser/ui/login/Connectors/VolleyCallBack.java
+++
@@ -1,6 +0,0 @@
-package com.example.music_compromiser.ui.login.Connectors;
-
-public interface VolleyCallBack {
-
-    void onSuccess();
-}
--- front/app/src/main/java/com/example/music_compromiser/ui/login/User.java
+++
@@ -1,9 +0,0 @@
-package com.example.music_compromiser.ui.login;
-
-public class User {
-    public String birthdate;
-    public String country;
-    public String display_name;
-    public String email;
-    public String id;
-}
