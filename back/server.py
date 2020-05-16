ef0035d068972422cb93a8b9dcedd0908657ac57
Ryan Bautista <Ryan.Bautista86@myhunter.cuny.edu>
2020-05-16 18:57:58 -0400

    Added MongoDB support

--- back/server.py
+++ back/server.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
 import json
 import classes
 from classes import Server
 import pik
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://bakainkorp:Zxcvbnm%2C.%2F1Zxcvbnm%2C.%2F1@musiccompromiser-s4phd.mongodb.net/test"
+mongo = PyMongo(app)
 app.config["DEBUG"] = False
+database = mongo.db.database
 
 @app.route('/', methods=['GET'])
 def landing():
@@ -20,6 +24,15 @@ def host():
         data = input['data'] #this should be the top tracks json that you get from the api call
         server = Server(host, data)
         output = {'serverid': server.serverid}
+        
+        #Used for temporary information storage between phones and routes
+        #ServerID prevents the database from making mistakes via duplicate
+        database.insert ({
+            "User" : host,
+            "ServerID" : server.serverid,
+            "Prep" : False
+        })
+        
         pik.save()
         return json.dumps(output)
     except Exception as e:
@@ -39,6 +52,13 @@ def join():
             if str(server.serverid) == serverid:
                 server.user_join(client, data)
                 success['success'] = 'True'
+                
+                database.insert ({
+                    "User" : client,
+                    "ServerID" : server.serverid,
+                    "Prep" : False
+                })
+                
                 pik.save()
                 return json.dumps(success)
         pik.save()
@@ -56,6 +76,23 @@ def start():
             if str(server.serverid) == str(serverid):
                 if len(server.complist) == 0:
                     server.make_comprimise()
+                
+                #fileString is the entire JSON file combined with all participating users
+                #Prep allows /select to find only items with JSON files attached to them
+                sameSID = database.find_one({'ServerID' : server.ServerID})
+                Users = []
+                while sameSID:
+                    Users.append(sameSID['User'])
+                    database.remove(sameSID)
+                    document = database.find_one({'ServerID' : server.ServerID})
+                fileString = json.dumps(server.complist)
+                database.insert ({
+                    "ServerID" : server.serverid,
+                    "JSON file" : fileString,
+                    "Users" : Users
+                    "Prep" : True
+                })
+                
                 pik.save()
                 return(json.dumps(server.complist))
         pik.save()
@@ -72,6 +109,45 @@ def show():
         out.append(server.serverid)
     pik.save()
     return json.dumps(out)
+    
+#User presses "Past Playlists" and the Server returns every playlist the User participated in
+#Returns userPlaylists, which is an array of all serverIDs, meaning different playlists
+@app.route('/select', methods=['POST'])
+def select(theUser):
+    try:
+        pik.load()
+        userPlaylists = []
+        
+        pastPlaylists = database.find({'Prep' : True})
+        for x in pastPlaylists:
+            check = x['Users']
+            while len(check) != 0:
+                if check[0] == theUser:
+                    userPlaylists.append(x['ServerID'])
+                check.pop(0)
+        
+        pik.save()
+        
+        return json.dumps(userPlaylists)
+    except Exception as e:
+        return str(e)
+        
+#User presses the desired playlist
+#Returns the JSON file in pastPlaylist, which has the same ID as the user's choice
+@app.route('/pastplay', methods=['POST'])
+def pastplay(theID):
+    try:
+        pik.load()
+        pastPlaylist = database.find_one({"$and":
+            [{'Prep' : True},
+            {'ServerID' : theID}]})
+        pik.save()
+        
+        return json.dumps(pastPlaylist['JSON file'])
+    except Exception as e:
+        return str(e)
+        
+        
 
 @app.route('/kill')
 def kill():
